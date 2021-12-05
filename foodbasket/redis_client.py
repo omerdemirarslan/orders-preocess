0139038180d236b11cff352aafc2b6acb4dcc1ca
Ömer DEMİRARSLAN <omerdemirarsln@gmail.com>
2021-12-05 23:48:53 +0300

    OD-08 Comment: Set Redis Integration and Necessary Files

---
+++ foodbasket/redis_client.py
@@ -0,0 +1,32 @@
+import json
+import redis
+
+
+class RedisClient:
+    def __init__(self):
+        self.redis_client = redis.StrictRedis(
+            host='redis',
+            port=6379,
+            db=1
+        )
+
+    def get_client(self):
+        """
+        This Method Return Redis Client
+        :return:
+        """
+        return self.redis_client
+
+    def publish_data_on_redis(self, json_data: dict, channel_name: str) -> bool:
+        """
+        Tis Method Publishing Data To Redis Client
+        :param json_data:
+        :param channel_name:
+        :return:
+        """
+        self.redis_client.publish(
+            channel=channel_name,
+            message=json.dumps(json_data)
+        )
+
+        return True
