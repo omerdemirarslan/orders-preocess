f32368b1630c668c73f6e3227880668257468063
Ömer DEMİRARSLAN <omerdemirarsln@gmail.com>
2021-12-06 21:03:04 +0300

    OD-09 Commet: Set The Files For Project Basic

--- foodbasket/settings.py
+++ foodbasket/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'orders',
     'partners',
-    'api',
     'rest_framework',
 ]
 
@@ -55,6 +54,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'PAGE_SIZE': 10
+}
+
 
 ROOT_URLCONF = 'foodbasket.urls'
 
