27c9afa4fa42dea80505b8a3dd3a31ee769fe51e
Ömer DEMİRARSLAN <omerdemirarsln@gmail.com>
2021-12-07 11:41:49 +0300

    OD-15 Commet: Fix The URL Path For Orders

--- orders/urls.py
+++ orders/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from .views import (SendOrderViewSet, OrderViewSet)
 
-app_name = 'api/v1/order'
+app_name = 'api/v1/orders'
 
 urlpatterns = [
-    path('create-order/', SendOrderViewSet.as_view({'get': 'list'}), name='create-order'),
-    path('get-order/', OrderViewSet.as_view({'get': 'list'}), name='order')
+    path('create-order/', SendOrderViewSet, name='create-order'),
+    path('get-order/', OrderViewSet, name='order')
 ]
