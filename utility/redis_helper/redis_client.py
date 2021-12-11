c89fda6f1402cd294bc3f54dfb44bff9c803314d
Ömer DEMİRARSLAN <omerdemirarsln@gmail.com>
2021-12-12 02:38:26 +0300

    OD-21 Comment: Fix Subscriber Service Name In The Dockercompose File and Constant Values In The Redis File

--- utility/redis_helper/redis_client.py
+++ utility/redis_helper/redis_client.py
@@ -4,10 +4,13 @@ import redis
 
 class RedisClient:
     def __init__(self):
+        self.host = "redis"
+        self.port = 6379
+        self.db = 1
         self.redis_client = redis.StrictRedis(
-            host='redis',
-            port=6379,
-            db=1
+            host=self.host,
+            port=self.port,
+            db=self.db
         )
 
     def publish_data_on_redis(self, json_data: dict, channel_name: str) -> bool:
